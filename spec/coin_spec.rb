b0b2a7ef2f1a1679409058eb92e89ef190fb5997
Alyssa Horrocks & Michael Bunker <student@epicodus.com>
2015-08-06 09:58:54 -0700

    adds spec for pennies and nickels.

--- spec/coin_spec.rb
+++ spec/coin_spec.rb
@@ -3,6 +3,10 @@ require('coin')
 
 describe('fixnum#coin') do
   it('Returns change needed in all pennies') do
-    expect((15).coin()).to(eq(15))
+    expect((15).coin()).to(eq("15 pennies"))
+  end
+
+  it('Returns change needed in pennies and nickels') do
+    expec((12).coin()).to(eq("2 nickels and 2 pennies"))
   end
 end
