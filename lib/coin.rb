9b91df7ac538e6d960db1a0831b585cbf089f99c
Alyssa Horrocks & Michael Bunker <student@epicodus.com>
2015-08-06 12:05:12 -0700

    Pass final spec for change with Quarters, Dimes, Nickels, and pennies.

--- lib/coin.rb
+++ lib/coin.rb
@@ -9,12 +9,10 @@ class String
 
     coin_values = {"Penny" => 1, "Nickel" => 5, "Dime" => 10, "Quarter" => 25}
 
-    counter = 0
-    change = 0
+    quarters_needed = change_needed./(25)
+    remainder = change_needed%25
 
-    if change_needed > 24
-      quarters_needed = change_needed./(25)
-      remainder = change_needed%25
+    if remainder > 0
         while remainder > 9
           dimes_needed = remainder./(10)
           remainder = remainder%10
@@ -24,19 +22,22 @@ class String
             remainder = remainder%5
             pennies_needed = remainder
           end
-
-      final_quarters = quarters_needed.to_s
-      final_dimes = dimes_needed.to_s
-      final_nickels = nickels_needed.to_s
-      final_pennies = pennies_needed.to_s
-      if final_pennies == "0"
-        final_quarters + final_dimes + final_nickels
-      else
-        final_quarters + final_dimes + final_nickels + final_pennies
-      end
     else
-      "Less than a quarter"
+      "You don't need change."
     end
-#dimes_needed
+
+    final_quarters = quarters_needed.to_s
+    final_dimes = dimes_needed.to_s
+    final_nickels = nickels_needed.to_s
+    final_pennies = pennies_needed.to_s
+
+    if final_quarters == "0"
+      final_dimes + final_nickels + final_pennies
+    elsif final_pennies == "0"
+      final_quarters + final_dimes + final_nickels
+    else
+      final_quarters + final_dimes + final_nickels + final_pennies
+    end
+
   end
 end
