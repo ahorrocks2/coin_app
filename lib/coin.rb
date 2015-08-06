e3b2eca3c4aae51f5d117af3aa15ca528301169b
Alyssa Horrocks & Michael Bunker <student@epicodus.com>
2015-08-06 14:50:05 -0700

    Refactors coin method

--- lib/coin.rb
+++ lib/coin.rb
@@ -1,43 +1,29 @@
 class Fixnum
   define_method(:coin) do
     change_needed = self
-    # if change_wanted < 1
-    #   change_needed = change_wanted.*100
-    # else
-    #   change_needed = change_wanted
-    # end
-
-    coin_values = {1 => "Penny", 5 => "Nickel", 10 => "Dime", 25 => "Quarter"}
 
     quarters_needed = change_needed./(25)
+    pennies_needed = 0
+    nickels_needed = 0
+    dimes_needed = 0
     remainder = change_needed % 25
 
-    if remainder > 0
-      while remainder > 9
-        dimes_needed = remainder./(10)
-        remainder = remainder % 10
-      end
-      while remainder > 4
-        nickels_needed = remainder./(5)
-        remainder = remainder % 5
-        pennies_needed = remainder
-      end
-    else
-      "You don't need change."
+    while remainder > 9
+      dimes_needed = remainder./(10)
+      remainder = remainder % 10
     end
 
-    final_quarters = quarters_needed.to_s
-    final_dimes = dimes_needed.to_s
-    final_nickels = nickels_needed.to_s
-    final_pennies = pennies_needed.to_s
-
-    if final_quarters == "0"
-      final_dimes + final_nickels + final_pennies
-    elsif final_pennies == "0"
-      final_quarters + final_dimes + final_nickels
-    else
-      final_quarters + final_dimes + final_nickels + final_pennies
+    while remainder > 4
+      nickels_needed = remainder./(5)
+      remainder = remainder % 5
+      pennies_needed = remainder
     end
-
+    
+    final_array = []
+    final_array.push(quarters_needed)
+    final_array.push(dimes_needed)
+    final_array.push(nickels_needed)
+    final_array.push(pennies_needed)
+    final_array[0].to_s + " quarters " + final_array[1].to_s + " dimes " + final_array[2].to_s + " nickels " + final_array[3].to_s + " pennies"
   end
 end
--- spec/coin_spec.rb
+++ spec/coin_spec.rb
@@ -3,18 +3,18 @@ require('coin')
 
 describe('fixnum#coin') do
   it('Returns change needed in all quarters') do
-    expect((25).coin()).to(eq("1"))
+    expect((25).coin()).to(eq("1 quarters 0 dimes 0 nickels 0 pennies"))
   end
 
   it('Returns change needed in quarters and dimes') do
-    expect((35).coin()).to(eq("11"))
+    expect((35).coin()).to(eq("1 quarters 1 dimes 0 nickels 0 pennies"))
   end
 
-  it('Returns change needed in pennies, nickels, and dimes') do
-    expect((40).coin()).to(eq("111"))
+  it('Returns change needed in quarters, nickels, and dimes') do
+    expect((40).coin()).to(eq("1 quarters 1 dimes 1 nickels 0 pennies"))
   end
 
   it('Returns change needed in quarters, dimes, nickels, and pennies.') do
-    expect((41).coin()).to(eq("1111"))
+    expect((41).coin()).to(eq("1 quarters 1 dimes 1 nickels 1 pennies"))
   end
 end
