189442211300db6d8d4b533b3b3c08c512556888
Alyssa Horrocks & Michael Bunker <student@epicodus.com>
2015-08-06 12:18:00 -0700

    Change method class from string to fixnum.

--- lib/coin.rb
+++ lib/coin.rb
@@ -1,27 +1,27 @@
-class String
+class Fixnum
   define_method(:coin) do
-    change_wanted = self.to_i
-    if change_wanted < 1
-      change_needed = change_wanted.*100
-    else
-      change_needed = change_wanted
-    end
+    change_needed = self
+    # if change_wanted < 1
+    #   change_needed = change_wanted.*100
+    # else
+    #   change_needed = change_wanted
+    # end
 
-    coin_values = {"Penny" => 1, "Nickel" => 5, "Dime" => 10, "Quarter" => 25}
+    coin_values = {1 => "Penny", 5 => "Nickel", 10 => "Dime", 25 => "Quarter"}
 
     quarters_needed = change_needed./(25)
-    remainder = change_needed%25
+    remainder = change_needed % 25
 
     if remainder > 0
-        while remainder > 9
-          dimes_needed = remainder./(10)
-          remainder = remainder%10
-        end
-          while remainder > 4
-            nickels_needed = remainder./(5)
-            remainder = remainder%5
-            pennies_needed = remainder
-          end
+      while remainder > 9
+        dimes_needed = remainder./(10)
+        remainder = remainder % 10
+      end
+      while remainder > 4
+        nickels_needed = remainder./(5)
+        remainder = remainder % 5
+        pennies_needed = remainder
+      end
     else
       "You don't need change."
     end
--- spec/coin_spec.rb
+++ spec/coin_spec.rb
@@ -3,18 +3,18 @@ require('coin')
 
 describe('fixnum#coin') do
   it('Returns change needed in all quarters') do
-    expect(("25").coin()).to(eq("1"))
+    expect((25).coin()).to(eq("1"))
   end
 
   it('Returns change needed in quarters and dimes') do
-    expect(("35").coin()).to(eq("11"))
+    expect((35).coin()).to(eq("11"))
   end
 
   it('Returns change needed in pennies, nickels, and dimes') do
-    expect(("40").coin()).to(eq("111"))
+    expect((40).coin()).to(eq("111"))
   end
 
   it('Returns change needed in quarters, dimes, nickels, and pennies.') do
-    expect(("41").coin()).to(eq("1111"))
+    expect((41).coin()).to(eq("1111"))
   end
 end
